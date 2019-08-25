e7d1cfbbda612ab4acc098c6bd04f82f75b8ab42
Atiq Gauri <gauriatiq@gmail.com>
2019-08-25 22:59:50 +0530

    Added Thread Input Option: We can select form app that how many thread will be used in back c++ process.

--- source/worker1.js
+++ source/worker1.js
@@ -1,6 +1,9 @@
 const testAddon = require('./tool-addon/addon.node');
-var i;
-i = testAddon.add(3,4);
-//console.log(i);
-postMessage(i);
-module.exports = testAddon;
+
+onmessage = function(e) {
+    var i;
+	i = testAddon.add(Number(e.data),4);
+	//console.log(i);
+	postMessage(i);
+	module.exports = testAddon;
+};
