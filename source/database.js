00506c35a812f8f1b07cba68f7facb59ffc1756c
Atiq Gauri <gauriatiq@gmail.com>
2019-10-28 15:05:46 +0530

    configure database form single file everywhere

---
+++ source/database.js
@@ -0,0 +1,15 @@
+var Dexie = require('dexie');
+
+var database = new Dexie("PasswordTool");
+
+database.version(1).stores({
+    Patterns: 'pattern,address,popularity'
+})
+
+database.open().catch(function(error){
+    console.log("ERROR: "+ error);
+});
+
+module.exports = {
+    database    
+}
