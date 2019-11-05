dbb3ea1399691f435d229b2a703ebea0ce6370cd
Atiq Gauri <gauriatiq@gmail.com>
2019-11-05 23:16:25 +0530

    added n-readline module to large files line by line without high memory consumption

--- source/databaseWorker.js
+++ source/databaseWorker.js
@@ -1,47 +1,33 @@
 const {database} = require('./database.js');
-const fileSystem = require('fs');
-const readline = require('readline');
-const stream = require('stream');
+const lineByLine = require('n-readlines');
 const workerAcknowledgement = "DONE";
-var duplicates, totalRecords;
+var duplicates=0, addedRecords=0, splitter;
 
 database.open().catch(function(error){
     console.error("ERROR: "+ error);
 });
 
-console.log('Database worker is started');
-
-async function test_database_operation(){
-    var result = await database.Patterns.where('pattern').equals('This/Is/Just/For/Test/').toArray()
-    totalRecords = totalRecords - duplicates;
-    var str = "Duplicates " + duplicates.toString() + " Added " + totalRecords.toString();
-    postMessage(str);
-}
-
-var instream = fileSystem.createReadStream('data/Stats/Patterns.txt');
-var outstream = new stream;
-var readInterface = readline.createInterface(instream, outstream);
-var splitter;
-
-database.transaction('rw', database.Patterns, () => {
-    duplicates = 0;
-    totalRecords=0;
-    readInterface.on('line', function(line) {
-        totalRecords++;
-        splitter = line.split('<|>');
-        database.Patterns.add({
-            pattern: splitter[0],
-            address: splitter[1],
-            popularity: splitter[2]
-        }).catch(function (e) {
-            console.log(e.message);
-            duplicates++;
-        });
+const liner = new lineByLine('./data/Stats/Patterns.txt');
+let line;
+let lineNumber = 0;
+database.transaction('rw', database.Patterns, async () => {
+  while (line = liner.next()) {
+    splitter = line.toString().split('<|>');
+    await database.Patterns.add({
+      pattern: splitter[0],
+      address: splitter[1],
+      popularity: splitter[2]
+    }).then(function(){
+      addedRecords++ 
+      lineNumber++; 
+    }).catch(function (e) {
+      console.log(e.message);
+      duplicates++;
+      lineNumber++;
     });
-}).catch(function (e) {
-    console.error("ERROR: "+ e);
-});
-
-readInterface.on('close', function() {
-    test_database_operation();
+  }
+}).then(function(){
+  postMessage("Duplicates " + duplicates.toString() + " Added " + addedRecords.toString());//use when you are using a web worker
+}).catch(error => {
+  console.error(error);
 });
