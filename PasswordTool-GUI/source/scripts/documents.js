20207bc33e9c0a5299c2b3ca5ac13ddd96ab5f28
Atiq Gauri <gauriatiq@gmail.com>
2020-02-19 15:18:17 +0530

    documents tab ui update and removed extra font files

--- PasswordTool-GUI/source/scripts/documents.js
+++ PasswordTool-GUI/source/scripts/documents.js
@@ -8,27 +8,40 @@ function get_back_button(){
 function show_document1(){
     frame = document.createElement('iframe');
     frame.classList.add("documentFrame");
-    frame.src = "https://drive.google.com/file/d/1gWjTW6tEVYis6erLceJoNPuVmvvN6kCh/preview";
+    frame.src = "https://drive.google.com/open?id=1Mz1t8SMHS4BnHrtkYlQtT8WRx4q6cx4f";
     document.getElementById("documentsContentContainer").appendChild(frame);
-    console.log('iframe.contentWindow =');
     backButton.style.display = "block";
 }
 
 function show_document2(){
     frame = document.createElement('iframe');
     frame.classList.add("documentFrame");
-    frame.src = "https://drive.google.com/file/d/1GRiRnMCJiTEHLmRm5xOjE9azLnACwEVw/view?usp=sharing";
+    frame.src = "https://drive.google.com/open?id=1GRiRnMCJiTEHLmRm5xOjE9azLnACwEVw";
     document.getElementById("documentsContentContainer").appendChild(frame);
-    console.log('iframe.contentWindow =');
     backButton.style.display = "block";
 }
 
 function show_document3(){
     frame = document.createElement('iframe');
     frame.classList.add("documentFrame");
-    frame.src = "https://drive.google.com/file/d/1gWjTW6tEVYis6erLceJoNPuVmvvN6kCh/preview";
+    frame.src = "https://drive.google.com/open?id=1Z1pqc6J2vR_eBht7YAhjZcoDAaXYHIsH";
     document.getElementById("documentsContentContainer").appendChild(frame);
-    console.log('iframe.contentWindow =');
+    backButton.style.display = "block";
+}
+
+function show_document4(){
+    frame = document.createElement('iframe');
+    frame.classList.add("documentFrame");
+    frame.src = "https://drive.google.com/open?id=1MqJOxsgZg2UfjK2jFwUT2M7aTcAli-sM";
+    document.getElementById("documentsContentContainer").appendChild(frame);
+    backButton.style.display = "block";
+}
+
+function show_document5(){
+    frame = document.createElement('iframe');
+    frame.classList.add("documentFrame");
+    frame.src = "https://drive.google.com/open?id=1slBybO6r5fui6M6tQ68gKKiPmKqgCgQ4";
+    document.getElementById("documentsContentContainer").appendChild(frame);
     backButton.style.display = "block";
 }
 
