b736305017d6d3cc1b1861b157feb423dfc22c08
Atiq Gauri <gauriatiq@gmail.com>
2020-03-16 13:51:46 +0530

    ignore

--- PasswordTool_GUI/source/threadWorkers/statisticsWorker.js
+++ PasswordTool_GUI/source/threadWorkers/statisticsWorker.js
@@ -2,6 +2,8 @@
 //REQUIRE NATIVE C++ ADDON
 const testAddon = require('../tool_addon/addon.node');
 
+
+
 //CALL C++ ANALYZATION API FUNCTION 
 var j = testAddon.stats_generator_api(2);
 //console.log(j);
